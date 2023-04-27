80c1915c1b581c2fbae09ba10e17219adf8f8b33
agustin1019 <agustin.mansilla@heritas.com.ar>
2023-04-27 19:30:48 -0300

    Agrega contenido sección About

--- veterinariaPerrofeliz/src/views/About.vue
+++ veterinariaPerrofeliz/src/views/About.vue
@@ -1,10 +1,51 @@
-<script setup>
-</script>
-
 <template>
-  <p class="text-5xl text-center text-gray-700 py-10">SECCION ABOUT</p>
-  
+    <span class="w-100">
+        <p class="text-5xl text-center text-gray-700 py-10">SECCION ABOUT</p>
+        <div class="grid grid-cols-1 w-100">
+            <div v-for="(servicio, index) in servicios" :key="index" class="grid grid-cols-2 place-items-center">
+                <div :class="index % 2 === 0 ? 'order-first sm:order-last' : 'order-last sm:order-first'" class="">
+                    <img :src="servicio.image" alt="imagen" class="object-cover w-full h-full" />
+                </div>
+                <div :class="index % 2 === 0 ? 'order-last sm:order-first' : 'order-first sm:order-last'" class="">
+                    <h1 class="text-4xl text-center text-gray-600 font-bold my-5">{{ servicio.title }}</h1>
+                    <p class="text-xl mx-5 text-gray-500 whitespace-pre-wrap leading-10">{{ servicio.text }}</p>
+                </div>
+            </div>
+        </div>
+
+    </span>
 </template>
 
-<style scoped>
-</style>
+<script>
+
+export default {
+    name: "GridComponent",
+    data() {
+        return {
+            servicios: [
+                {
+                    title: "Quiénes Somos",
+                    text: "Perro Feliz es una cadena de veterinarias en crecimiento que proporciona el cuidado de sus mascotas, diseñado para los propietarios de perros siempre activos.",
+                    image: "https://img.freepik.com/free-photo/cute-dog-during-consultation_23-2149314348.jpg?w=740&t=st=1682631642~exp=1682632242~hmac=0bfac98385e019584114435bccb5740b45c44b112f79aeb4ca0d2b1610397ec1",
+                },
+                {
+                    title: "Misión",
+                    text: "Nuestra misión es brindar servicios veterinarios de alta calidad. Nos enfocamos en la prevención, diagnóstico y tratamiento de enfermedades en las mascotas, con el objetivo de prolongar su vida y mejorar su bienestar.",
+                    image: "https://img.freepik.com/free-photo/veterinarian-taking-care-pet-dog_23-2149198648.jpg?w=740&t=st=1682631742~exp=1682632342~hmac=4552d2290c3a76975357662901ff73c1e4b2b98ce94560fbedf22975dff07068",
+                },
+                {
+                    title: "Filosofia",
+                    text:
+                        `Nos comprometemos con la educación continua y el perfeccionamiento de técnicas y conocimientos, para ofrecer siempre una atención médica calificada y de calidad a nuestros clientes.\nTenemos una mirada protectora en cuanto a la fauna silvestre de nuestra provincia, realizamos colaboraciones y aportes para la conservación de la misma.Nuestra empresa es una asociación de respetados profesionales veterinarios de la región  que trabajan fuertemente priorizando la calidad del servicio.`,
+                    image: "https://img.freepik.com/premium-photo/medical-student-smiling-camera-during-class_13339-297488.jpg?w=740",
+                },
+                {
+                    title: "Nuestra Visión",
+                    text: `Dentro de los objetivos a largo plazo de Perro Feliz tenemos:\n\t\tExpandirnos para ofrecer a nuestros clientes una nueva forma de atención especializada y de alta tecnología a nuestros queridos caninos.\n\t\tSer la veterinaria líder en ofrecer servicios de salud preventiva y tratamiento de enfermedades en animales, trabajando en colaboración con nuestros clientes para lograr una vida saludable para sus mascotas.\n\t\tImplementar un sistema de consultas virtuales para abarcar todas las distintas zonas de la provincia a través de una aplicación móvil, que permita reuniones a distancia y consultoría.\n\t\tExpandir nuestros servicios para incluir la atención médica a animales exóticos.`,
+                    image: "https://img.freepik.com/premium-photo/young-veterinarian-smiling-while-using-digital-tablet-near-cat-sits-table-vet-clinic_8595-20484.jpg?w=740",
+                },
+            ],
+        };
+    },
+};
+</script>
