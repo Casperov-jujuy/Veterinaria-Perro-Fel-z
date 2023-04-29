f539ea48af8acab62e44f9f6b5a4e49977636719
hernan360 <hernan360flores@gmail.com>
2023-04-29 17:44:39 -0300

    modifique el formulario

--- veterinariaPerrofeliz/src/views/ContactView.vue
+++ veterinariaPerrofeliz/src/views/ContactView.vue
@@ -1,187 +1,207 @@
 <template>
-  <!-- ----------------SECCION CONTACTO-------------------- -->
+  <!-- ----------------SECCION FORMULARIO CONTACTO-------------------- -->
 
-  <section class="pb-20 block">
-    <div class="container mx-auto px-4 lg:pt-24 lg:pb-64">
-      <div class="flex flex-wrap text-center justify-center">
-        <div class="w-full lg:w-6/12 px-4">
-          <h2 class="text-4xl font-semibold text-black">CONTACTO</h2>
-          <p class="text-lg leading-relaxed mt-4 mb-4 text-neutral-500">
-            Envianos tu consulta, nuestro grupo de trabajo le respondera en la
-            brevedad. Muchas gracias!!!
-          </p>
-        </div>
-      </div>
-    </div>
-  </section>
-  <section class="block pt-24 lg:pt-0">
-    <div class="container mx-auto px-4">
-      <div class="flex flex-wrap justify-center lg:-mt-64 -mt-48">
-        <div class="w-full lg:w-6/12 px-4">
-          <div
-            class="flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300"
-          >
-            <div class="flex-auto p-5 lg:p-10">
-              <h4 class="text-2xl font-semibold text-blueGray-500">
-                Complete este formulario, nos pondremos en contacto con usted en
-                24 horas.
-              </h4>
-              <div class="w-full mb-3 mt-8">
-                <label
-                  class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                  for="full-name"
-                  >Nombre Completo</label
-                ><input
-                  type="text"
-                  class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                  placeholder="Apellido, Nombre"
-                />
-              </div>
-              <div class="w-full mb-3">
-                <label
-                  class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                  for="email"
-                  >Email</label
-                ><input
-                  type="email"
-                  class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                  placeholder="Email"
-                />
-              </div>
-              <div class="w-full mb-3">
-                <label
-                  class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                  for="message"
-                  >Consulta</label
-                ><textarea
-                  rows="4"
-                  cols="80"
-                  class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
-                  placeholder="Escriba su consulta aqui..."
-                ></textarea>
-              </div>
-              <div class="text-center mt-6">
-                <button
-                  class="bg-slate-800 text-white text-sm font-bold border-0 py-2 px-6 focus:outline-none hover:bg-slate-500 shadow rounded text-lg my-3 mr-1 mb-1"
-                >
-                  Enviar
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+  <br />
+  <br />
+  <br />
 
-  <!-- ----------------SECCION COMENTARIOS REALIZADOS-------------------- -->
-  <div
-    class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-4xl md:mb-10 mt-20"
-  >
-    <h2
-      class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto"
-    >
-      <span class="relative inline-block">
-        <svg
-          viewBox="0 0 52 24"
-          fill="currentColor"
-          class="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+  <div class="max-w-screen-lg mx-auto p-5 ">
+    <div class="grid grid-cols-1 md:grid-cols-12 border rounded-r-lg">
+      <div class="bg-cyan-500 md:col-span-4 p-10 text-white rounded-l-lg">
+        <p class="mt-4 text-sm leading-7 font-regular uppercase">Contacto</p>
+        <h3
+          class="text-3xl sm:text-4xl leading-normal font-extrabold tracking-tight"
         >
-          <defs>
-            <pattern
-              id="1d4040f3-9f3e-4ac7-b117-7d4009658ced"
-              x="0"
-              y="0"
-              width=".135"
-              height=".30"
-            >
-              <circle cx="1" cy="1" r=".7"></circle>
-            </pattern>
-          </defs>
-          <rect
-            fill="url(#1d4040f3-9f3e-4ac7-b117-7d4009658ced)"
-            width="52"
-            height="24"
-          ></rect>
-        </svg>
-      </span>
-      Consultas realizadas por nuestros clientes
-    </h2>
-  </div>
+          Perro <span class="text-indigo-200">Feliz</span>
+        </h3>
+        <p class="mt-4 leading-7 text-gray-200 font-bold">
+          Hola, déjanos tus consultas que nuestro equipo se pondrá en contacto con Ud. respondiendo sus dudas en la brevedad. <br />
+          ¡Muchas gracias por elegir Veterinaria Perro Feliz!!
+        </p>
 
-  <div class="testimonial-1 py-4 md:py-12">
-    <div class="container mx-auto px-4">
-      <div class="md:flex md:flex-wrap md:-mx-4 mb-4">
-        <div class="md:w-1/2 md:px-4 mt-6 md:mt-0">
-          <div
-            class="testimonial p-6 border-2 border-solid flex hover:border-indigo-400 hover:bg-indigo-100 transition-colors duration-300"
+        <div class="flex items-center mt-5">
+          <svg
+            class="h-6 mr-2 text-zinc-50 "
+            fill="currentColor"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 489.536 489.536"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            enable-background="new 0 0 489.536 489.536"
           >
-            <div>
-              <p class="text-gray-600">
-                "It really saves me time and effort. FWR Bootstrap Blocks is
-                exactly what our business has been lacking. FWR Bootstrap Blocks
-                was worth a fortune to my company. FWR Bootstrap Blocks has
-                really helped our business. I'm good to go."
-              </p>
-              <div class="text-gray-900 font-bold uppercase mt-6">
-                - Jesse D.
-              </div>
-            </div>
-          </div>
+            <g>
+              <g>
+                <path
+                  d="m488.554,476l-99-280.2c-1-4.2-5.2-7.3-9.4-7.3h-45.6c12.9-31.1 19.6-54.9 19.6-70.8 0-64.6-50-117.7-112.5-117.7-61.5,0-112.5,52.1-112.5,117.7 0,17.6 8.2,43.1 19.9,70.8h-39.7c-4.2,0-8.3,3.1-9.4,7.3l-99,280.2c-3.2,10.3 6.3,13.5 9.4,13.5h468.8c4.2,0.5 12.5-4.2 9.4-13.5zm-246.9-455.3c51,1.06581e-14 91.7,43.7 91.7,96.9 0,56.5-79.2,182.3-91.7,203.1-31.3-53.1-91.7-161.5-91.7-203.1 0-53.1 40.6-96.9 91.7-96.9zm-216.7,448l91.7-259.4h41.7c29.9,64.1 83.3,151 83.3,151s81.4-145.7 83.8-151h47.4l91.7,259.4h-439.6z"
+                />
+                <rect width="136.5" x="177.054" y="379.1" height="20.8" />
+                <path
+                  d="m289.554,108.2c0-26-21.9-47.9-47.9-47.9s-47.9,21.9-47.9,47.9 20.8,47.9 47.9,47.9c27.1,0 47.9-21.8 47.9-47.9zm-75-1c0-14.6 11.5-27.1 27.1-27.1s27.1,12.5 27.1,27.1-11.5,27.1-27.1,27.1c-14.6,2.84217e-14-27.1-12.5-27.1-27.1z"
+                />
+              </g>
+            </g>
+          </svg>
+          <span class="text-sm"
+            >Nuemero #14, Calle xxx, Provincia xxx, Pais xxx.</span
+          >
         </div>
-
-        <div class="md:w-1/2 md:px-4 mt-6 md:mt-0">
-          <div
-            class="testimonial p-6 border-2 border-solid flex hover:border-indigo-400 hover:bg-indigo-100 transition-colors duration-300"
+        <div class="flex items-center mt-5 ">
+          <svg
+            class="h-6 mr-2 text-zinc-50  	"
+            fill="currentColor"
+            version="1.1"
+            id="Capa_1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            x="0px"
+            y="0px"
+            viewBox="0 0 60.002 60.002"
+            style="enable-background: new 0 0 60.002 60.002"
+            xml:space="preserve"
           >
-            <div>
-              <p class="text-gray-600">
-                "Needless to say we are extremely satisfied with the results.
-                Keep up the excellent work. Your company is truly upstanding and
-                is behind its product 100%. Thanks guys, keep up the good work!"
-              </p>
-              <div class="text-gray-900 font-bold uppercase mt-6">
-                - Annabell M.
-              </div>
-            </div>
-          </div>
+            <g>
+              <path
+                d="M59.002,37.992c-3.69,0-6.693-3.003-6.693-6.693c0-0.553-0.447-1-1-1s-1,0.447-1,1c0,4.794,3.899,8.693,8.693,8.693
+		c0.553,0,1-0.447,1-1S59.554,37.992,59.002,37.992z"
+              />
+              <path
+                d="M58.256,35.65c0.553,0,1-0.447,1-1s-0.447-1-1-1c-0.886,0-1.605-0.72-1.605-1.605c0-0.553-0.447-1-1-1s-1,0.447-1,1
+		C54.65,34.033,56.267,35.65,58.256,35.65z"
+              />
+              <path
+                d="M20.002,2.992c3.691,0,6.693,3.003,6.693,6.693c0,0.553,0.448,1,1,1s1-0.447,1-1c0-4.794-3.9-8.693-8.693-8.693
+		c-0.552,0-1,0.447-1,1S19.449,2.992,20.002,2.992z"
+              />
+              <path
+                d="M19.748,6.334c0,0.553,0.448,1,1,1c0.885,0,1.604,0.72,1.604,1.605c0,0.553,0.448,1,1,1s1-0.447,1-1
+		c0-1.988-1.617-3.605-3.604-3.605C20.196,5.334,19.748,5.781,19.748,6.334z"
+              />
+              <path
+                d="M44.076,37.889c-1.276-0.728-2.597-0.958-3.721-0.646c-0.844,0.234-1.532,0.768-1.996,1.546
+		c-1.02,1.22-2.286,2.646-2.592,2.867c-2.367,1.604-4.25,1.415-6.294-0.629L17.987,29.542c-2.045-2.045-2.233-3.928-0.631-6.291
+		c0.224-0.31,1.65-1.575,2.87-2.596c0.778-0.464,1.312-1.152,1.546-1.996c0.311-1.123,0.082-2.444-0.652-3.731
+		c-0.173-0.296-4.291-7.27-8.085-9.277c-1.926-1.019-4.255-0.669-5.796,0.872L4.7,9.059c-4.014,4.014-5.467,8.563-4.321,13.52
+		c0.956,4.132,3.742,8.529,8.282,13.068l14.705,14.706c5.762,5.762,11.258,8.656,16.298,8.656c3.701,0,7.157-1.562,10.291-4.695
+		l2.537-2.537c1.541-1.541,1.892-3.87,0.872-5.796C51.356,42.186,44.383,38.069,44.076,37.889z M51.078,50.363L48.541,52.9
+		c-6.569,6.567-14.563,5.235-23.76-3.961L10.075,34.233c-4.271-4.271-6.877-8.344-7.747-12.104
+		c-0.995-4.301,0.244-8.112,3.786-11.655l2.537-2.537c0.567-0.566,1.313-0.862,2.07-0.862c0.467,0,0.939,0.112,1.376,0.344
+		c3.293,1.743,7.256,8.454,7.29,8.511c0.449,0.787,0.62,1.608,0.457,2.196c-0.1,0.36-0.324,0.634-0.684,0.836l-0.15,0.104
+		c-0.853,0.712-2.883,2.434-3.308,3.061c-0.612,0.904-1.018,1.792-1.231,2.665c-0.711-1.418-1.286-3.06-1.475-4.881
+		c-0.057-0.548-0.549-0.935-1.098-0.892c-0.549,0.058-0.949,0.549-0.892,1.099c0.722,6.953,6.129,11.479,6.359,11.668
+		c0.025,0.02,0.054,0.028,0.08,0.045l10.613,10.613c0.045,0.045,0.092,0.085,0.137,0.129c0.035,0.051,0.058,0.108,0.104,0.154
+		c0.189,0.187,4.704,4.567,11.599,5.283c0.035,0.003,0.07,0.005,0.104,0.005c0.506,0,0.94-0.383,0.994-0.896
+		c0.057-0.55-0.342-1.041-0.892-1.099c-2.114-0.219-3.987-0.839-5.548-1.558c0.765-0.23,1.543-0.612,2.332-1.146
+		c0.628-0.426,2.35-2.455,3.061-3.308l0.104-0.151c0.202-0.359,0.476-0.583,0.836-0.684c0.588-0.159,1.409,0.008,2.186,0.45
+		c0.067,0.04,6.778,4.003,8.521,7.296C52.202,48.062,51.994,49.447,51.078,50.363z"
+              />
+            </g>
+          </svg>
+          <span class="text-sm">+54 xxx xx xx xx</span>
         </div>
-
-        <div class="md:w-1/2 md:px-4 mt-6">
-          <div
-            class="testimonial p-6 border-2 border-solid flex hover:border-indigo-400 hover:bg-indigo-100 transition-colors duration-300"
+        <div class="flex items-center mt-5">
+          <svg
+            class="h-6 mr-2 text-zinc-50	"
+            fill="currentColor"
+            version="1.1"
+            id="Capa_1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            x="0px"
+            y="0px"
+            viewBox="0 0 300.988 300.988"
+            style="enable-background: new 0 0 300.988 300.988"
+            xml:space="preserve"
           >
-            <div>
-              <p class="text-gray-600">
-                "FWR Bootstrap Blocks is exactly what our business has been
-                lacking. FWR Bootstrap Blocks is both attractive and highly
-                adaptable. Man, this thing is getting better and better as I
-                learn more about it."
-              </p>
-              <div class="text-gray-900 font-bold uppercase mt-6">
-                - Candace H.
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="md:w-1/2 md:px-4 mt-6">
-          <div
-            class="testimonial p-6 border-2 border-solid flex hover:border-indigo-400 hover:bg-indigo-100 transition-colors duration-300"
-          >
-            <div>
-              <p class="text-gray-600">
-                "This is simply unbelievable! I use FWR Bootstrap Blocks often.
-                You've saved our business! No matter where you go, FWR Bootstrap
-                Blocks is the coolest, most happening thing around!"
-              </p>
-              <div class="text-gray-900 font-bold uppercase mt-6">- Joe H.</div>
-            </div>
-          </div>
+            <g>
+              <path
+                d="M150.494,0.001C67.511,0.001,0,67.512,0,150.495s67.511,150.493,150.494,150.493s150.494-67.511,150.494-150.493
+                S233.476,0.001,150.494,0.001z M150.494,285.987C75.782,285.987,15,225.206,15,150.495S75.782,15.001,150.494,15.001
+                s135.494,60.782,135.494,135.493S225.205,285.987,150.494,285.987z"
+              />
+              <polygon
+                points="142.994,142.995 83.148,142.995 83.148,157.995 157.994,157.995 157.994,43.883 142.994,43.883 		"
+              />
+            </g>
+          </svg>
+          <span class="text-sm">24/7</span>
         </div>
       </div>
+      <form class="md:col-span-8 p-10 bg-cyan-50">
+        <div class="flex flex-wrap -mx-3 mb-6">
+          <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label
+              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="grid-first-name"
+            >
+              Apellido
+            </label>
+            <input
+              class="appearance-none bg-transparent border-b border-indigo-500 w-full text-gray-700 mr-3 p-4 leading-tight focus:outline-none focus:bg-gray-200"
+              type="text"
+              placeholder="Apllido"
+              aria-label="Full name"
+            />
+          </div>
+          <div class="w-full md:w-1/2 px-3">
+            <label
+              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="grid-last-name"
+            >
+              Nombre
+            </label>
+            <input
+              class="appearance-none bg-transparent border-b border-indigo-500 w-full text-gray-700 mr-3 p-4 leading-tight focus:outline-none focus:bg-gray-200"
+              type="text"
+              placeholder="Nombre"
+              aria-label="Full name"
+            />
+          </div>
+        </div>
+        <div class="flex flex-wrap -mx-3 mb-6">
+          <div class="w-full px-3">
+            <label
+              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="grid-password"
+            >
+              Email
+            </label>
+            <input
+              class="appearance-none bg-transparent border-b border-indigo-500 w-full text-gray-700 mr-3 p-4 leading-tight focus:outline-none focus:bg-gray-200"
+              type="email"
+              placeholder="jane@exp.com"
+              aria-label="Full name"
+            />
+          </div>
+        </div>
+
+        <div class="flex flex-wrap -mx-3 mb-6">
+          <div class="w-full px-3">
+            <label
+              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="grid-password "
+            >
+              Consulta
+            </label>
+            <textarea
+              rows="10"
+              class="appearance-none bg-transparent border-b border-indigo-500 w-full text-gray-700 mr-3 p-4 focus:bg-gray-200 leading-tight focus:outline-none"
+              aria-label="Full name"
+            >
+            </textarea>
+          </div>
+          <div class="flex justify-end w-full px-5 py-5">
+            <button
+              class="bg-cyan-500 text-white text-sm font-bold border-0 py-2 px-6 focus:outline-none hover:bg-slate-500 shadow rounded text-lg my-3 mr-1 mb-1"
+            >
+              Enviar
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
   </div>
+  <br>
+  <br>
 
   <!---------------------- separador -------------------------------------->
   <div
@@ -193,6 +213,7 @@
   </div>
 
   <!-- --------------------gps----------------- -->
+ 
   <section class="text-gray-600 body-font relative my-10 mx-10">
     <div class="absolute inset-0 bg-gray-300 mx-7">
       <iframe
@@ -211,21 +232,21 @@
         class="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-3 mt-10 md:mt-0 relative z-10 shadow-md"
       >
         <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">
-          COMO LLEGAR
+          Como llegar
         </h2>
         <div class="relative mb-4">
           <label for="email" class="leading-7 text-sm text-gray-600"
-            >Su ubicacion actual</label
+            >Ingrese su ubicación actual</label
           >
           <input
             type="text"
             class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
           />
           <button
-            class="bg-slate-800 text-white border-0 py-2 px-6 focus:outline-none hover:bg-slate-500 rounded text-lg my-3"
-          >
-            Buscar
-          </button>
+              class="bg-cyan-500 text-white text-sm font-bold border-0 py-2 px-6 focus:outline-none hover:bg-slate-500 shadow rounded text-lg my-3 mr-1 mb-1"
+            >
+              Buscar
+            </button>
         </div>
       </div>
     </div>
